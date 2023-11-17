de1ef4f24ed4d3657c0de464f1d70703313cd2b8
Andreas-206 <puma_real@ukr.net>
2023-11-17 14:25:30 +0200

    1

--- task-2.js
+++ task-2.js
@@ -24,10 +24,3 @@ console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator",
 
 storage.removeItem("Prolonger");
 console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
-
-const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
-console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
-storage.addItem("Droid");
-console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
-storage.removeItem("Prolonger");
-console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
